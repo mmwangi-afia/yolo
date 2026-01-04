ff9dbc7aa2637c4f924f145095abd90d5e5c7867
mwuthonis <muthonishelmith136@gmail.com>
2026-01-04 19:27:06 +0300

    Connecting the backend to MongoDB cluster2

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ let mongodb_url = 'mongodb://localhost/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://muthonishelmith136_db_user:Z5rnbiWhUXEy8Y04@cluster2.mywjaji.mongodb.net/?appName=Cluster2';
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
